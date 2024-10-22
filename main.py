ace89dfbdddadf39bc9e2b820805143fa4e39c13
miguel <jmiguel.hdez@gmail.com>
2024-10-21 23:33:20 -0500

    Print a Report first try

--- main.py
+++ main.py
@@ -12,12 +12,18 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def print_report(num_words, char_dict):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document\n")
+    for c in char_dict:
+        print(f"The {c} character was found {char_dict[c]} times")
+    print("--- End report ---")
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     words_num = get_word_count(text)
-    print(f"words found in book {words_num}")
-    print(get_char_count(text))
+    char_dict = get_char_count(text)
+    print_report(words_num, char_dict)
 
 main()
