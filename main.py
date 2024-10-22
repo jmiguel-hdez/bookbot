a960311bdc02540bf38b834faa63bfb23977df25
miguel <jmiguel.hdez@gmail.com>
2024-10-21 23:49:06 -0500

    Print a Report: after looking at hints

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def get_char_count(text):
     char_counts = {}
     for c in text.lower():
-        char_counts[c] = char_counts.get(c,0) + 1
+        if c.isalpha():
+            char_counts[c] = char_counts.get(c,0) + 1
     return char_counts
 
 def get_word_count(text):
@@ -12,11 +13,21 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def print_report(num_words, char_dict):
+def sort_on(dict):
+    return dict["num"]
+
+def convert_to_list(dict):
+    new_list = []
+    for k in dict:
+        new_list.append({"name":k, "num":dict[k]})
+    new_list.sort(reverse=True, key=sort_on)
+    return new_list
+
+def print_report(num_words, lst):
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"{num_words} words found in the document\n")
-    for c in char_dict:
-        print(f"The {c} character was found {char_dict[c]} times")
+    for d in lst:
+        print(f"The '{d['name']}' character was found {d['num']} times")
     print("--- End report ---")
 
 def main():
@@ -24,6 +35,7 @@ def main():
     text = get_book_text(book_path)
     words_num = get_word_count(text)
     char_dict = get_char_count(text)
-    print_report(words_num, char_dict)
+    dict_list = convert_to_list(char_dict)
+    print_report(words_num, dict_list)
 
 main()
